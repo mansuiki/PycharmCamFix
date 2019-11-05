ed41aecce78ab038914d6f1038e82b4469e3e311
mansuiki <mansuiki@gmail.com>
2019-11-05 12:41:49 +0900

    Initial Commit

---
+++ pycharm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  pycharm
+//
+//  Created by GyeongTaek Kim on 05/11/2019.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
